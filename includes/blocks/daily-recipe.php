4606a8c135c637077aaff9a4cf6609047a3ed74b
Nick Brown <Nickb6437@outlook.com>
2024-01-14 07:51:26 +0000

    Section 14 - lesson 280

--- includes/blocks/daily-recipe.php
+++ includes/blocks/daily-recipe.php
@@ -1,9 +1,24 @@
 <?php 
 
   function bwd_plus_daily_recipe_render_cb($atts) {
+    $title = esc_html($atts['title']);
+    $id = get_transient('bwd_plus_daily_recipe_id');
+
+    if(!$id) {
+      $id = bwd_plus_generate_daily_recipe();
+    }
+
     ob_start();
     ?>
-
+      <div class="wp-block-bwd-plus-daily-recipe">
+        <h6><?= $title; ?></h6>
+        <a href="<?= the_permalink($id); ?>">
+          <img 
+            src="<?= get_the_post_thumbnail_url($id, 'full') ?>"
+            alt="featured post img">
+            <h3><?= get_the_title($id); ?></h3>
+        </a>
+      </div>
     <?php
     $output = ob_get_contents();
     ob_end_clean();
